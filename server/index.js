5b0841d5c829343ac6d5f4998488a65b329f97b2
rvierbuc <ranlikerats@gmail.com>
2023-11-03 10:57:12 -0500

    added a basic server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
 // server index to set up port listen 
+const express = require('express')
+const app = express()
+    const PORT = 3000
+
+app.get('/',(req, res)=>{
+    res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`)
+})
