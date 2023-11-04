9e353b3cad0bb88a6b1163cb96f892baf8c5b8dd
danielalejandrojaen <danielalejandrojaen@gmail.com>
2023-11-04 18:21:29 -0500

    add get method in server/app.js to /leaderboards endpoint to retrieve all the users and return them in order by high score, also updated seed data with different numbers to help with testing

--- server/app.js
+++ server/app.js
@@ -14,4 +14,24 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(clientPath, 'index.html'));
 });
 
+//get the leaderboard from the database
+app.get('/leaderboard', (req, res) => {
+  //use the User model to findAll users in the database
+  User.findAll({
+    //order them by highscore in descending order
+    order: [['highscore', 'DESC']],
+    //limit the results to 10
+    limit: 10,
+  })
+  //then take the users 'leaderboard' and send them back to the client
+    .then((users) => {
+      // send back the users
+      res.send(users);
+    })
+    // error handling
+    .catch((err) => {
+      console.error('Unable to get leaderboard:', err);
+    });
+});
+
 module.exports = app;
