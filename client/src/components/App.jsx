66d9d7abd5f02197951af984b91376980a43d733
alexmbeasley <alexmbeasley@gmail.com>
2023-11-04 16:29:30 -0500

    google oauth working

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,9 @@
-import React, { useState } from 'react';
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable func-style */
+import React, { useState, useEffect } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { jwtDecode } from 'jwt-decode';
+
 import LeaderBoard from './LeaderBoard';
 import Profile from './Profile';
 import Play from './Play';
@@ -8,22 +13,63 @@ import Login from './Login';
 // Parent App comp
 const App = () => {
   const [view, setView] = useState('Login');
+  const [user, setUser] = useState({});
+
+  // eslint-disable-next-line func-style
+  function handleCallbackResponse(response) {
+    console.log('Encoded JWT info:', response.credential);
+
+    const userObj = jwtDecode(response.credential);
+    console.log('Decoded JWT info:', userObj);
+
+    setUser(userObj);
+    document.getElementById('signInDiv').hidden = true;
+  }
+
+  function handleSignOut() {
+    setUser({});
+    document.getElementById('signInDiv').hidden = false;
+  }
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: '904896192288-1ds4bgshm1iticdll13184fprmepj7l3.apps.googleusercontent.com',
+      callback: handleCallbackResponse,
+    });
+    google.accounts.id.renderButton(
+      document.getElementById('signInDiv'),
+      {
+        theme: 'outline',
+        size: 'extraLarge',
+        width: 309,
+        text: 'continue_with',
+      },
+    );
+
+    google.accounts.id.prompt();
+  }, []);
+
   return (
     <div>
-      {/* this is rendering in this commit */}
-      <nav>
-        <button type="button" onClick={() => setView('LeaderBoard')}>Leaderboard</button>
-        <button type="button" onClick={() => setView('Profile')}>Profile</button>
-        <button type="button" onClick={() => setView('Play')}>Play</button>
-        <button type="button" onClick={() => setView('QuestionBuilder')}>Create A Quiz</button>
-        <button type="button" onClick={() => setView('Login')}>Login</button>
-        <button type="button" onClick={() => setView('Login')}>Logout</button>
-      </nav>
-      {view === 'LeaderBoard' && <LeaderBoard />}
-      {view === 'Profile' && <Profile />}
-      {view === 'Play' && <Play />}
-      {view === 'QuestionBuilder' && <QuestionBuilder />}
-      {view === 'Login' && <Login />}
+      <div id="signInDiv" />
+      {Object.keys(user).length !== 0 && (
+        <div>
+          <nav>
+            <button type="button" onClick={() => setView('LeaderBoard')}>Leaderboard</button>
+            <button type="button" onClick={() => setView('Profile')}>Profile</button>
+            <button type="button" onClick={() => setView('Play')}>Play</button>
+            <button type="button" onClick={() => setView('QuestionBuilder')}>Create A Quiz</button>
+          </nav>
+          {view === 'LeaderBoard' && <LeaderBoard />}
+          {view === 'Profile' && <Profile />}
+          {view === 'Play' && <Play />}
+          {view === 'QuestionBuilder' && <QuestionBuilder />}
+          {view === 'Login' && <Login />}
+          <img src={user.picture} />
+          <h2>{user.name}</h2>
+          <button type="button" onClick={handleSignOut}>Logout</button>
+        </div>
+      )}
     </div>
   );
 };
