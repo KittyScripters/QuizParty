23febf7af047635695a25615ca2bc46d9264fe01
SNelson723 <stephenn649@gmail.com>
2023-11-04 13:01:49 -0500

    Added hardcoded sample data to Profile.jsx, table containing user's followers, stats, achievements, and server request handling to GET all users and user by id which are working in postman => status code 200

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 // user's profile
 const Profile = () => {
+  // user object state variable
+  const getUser = () => {
+    axios.get('/api/users')
+  };
+
   return (
     <div>
       <img alt="user" />
@@ -10,8 +16,19 @@ const Profile = () => {
         User bio paragraph describing themselves
       </p>
       <div>
-        <span>Their followers</span>
-        <span>Personal High Score</span>
+        <table>
+          <tr className="headers">
+            <th className="profile-table">Followers</th>
+            <th className="profile-table" colSpan="2">Stats</th>
+            <th className="profile-table">Achievements</th>
+          </tr>
+          <tr className="table-data">
+            <td>Stephen</td>
+            <td>Entertainment: Music</td>
+            <td>44</td>
+            <td>Top Dog: Description</td>
+          </tr>
+        </table>
       </div>
       <div>
         <p>List of their questions from the database</p>
--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable object-shorthand */
 const express = require('express');
 const path = require('path');
 const { db, User, Question } = require('./db/index');
@@ -14,10 +15,30 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(clientPath, 'index.html'));
 });
 
+//express get request to access all the users in the database
 app.get('/api/users', (req, res) => {
   User.findAll()
-    .then((users) => res.status(200).res.send(users))
-    .catch((err) => console.error('Could not GET users', err));
+    .then((data) => {
+      res.status(200);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.error('Could not GET users', err);
+    });
+});
+
+//get user by id => working in postman
+app.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  User.findOne({ where: { id: id } })
+    .then((user) => {
+      res.status(200);
+      res.send(user);
+    })
+    .catch((err) => {
+      console.log('id', req.params.id);
+      console.error('Could not GET user by id', err);
+    })
 });
 
 module.exports = app;
