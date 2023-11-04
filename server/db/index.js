7ee83b521906566576f8f643a736345e89b291af
rvierbuc <ranlikerats@gmail.com>
2023-11-04 11:56:31 -0500

    created seed data

--- server/db/index.js
+++ server/db/index.js
@@ -8,6 +8,7 @@ const db = new Sequelize({
   dialect: 'mysql',
   username: 'root',
   database: 'trivia',
+  password: '',
 });
 
 db.authenticate()
@@ -38,7 +39,7 @@ const User = db.define('user', {
   history_score: Sequelize.INTEGER,
   nature_score: Sequelize.INTEGER,
   politics_score: Sequelize.INTEGER,
-});
+}, { timesstamps: true });
 
 const Question = db.define('question', {
   id: {
@@ -51,12 +52,13 @@ const Question = db.define('question', {
     references: { model: User, key: 'id' }, 
   },
   question: Sequelize.STRING,
+  difficulty: Sequelize.STRING,
   category: Sequelize.STRING,
   correct_answer: Sequelize.STRING,
   incorrect_answer_1: Sequelize.STRING,
   incorrect_answer_2: Sequelize.STRING,
   incorrect_answer_3: Sequelize.STRING,
-});
+}, { timesstamps: true });
 
 const joinFollower = db.define('follower', {
   following_user_id: {
@@ -67,7 +69,7 @@ const joinFollower = db.define('follower', {
     type: Sequelize.INTEGER, 
     references: { model: User, key: 'id' }, 
   },
-});
+}, { timesstamps: true });
 
 const Achievement = db.define('achievement', {
   id: {
@@ -76,7 +78,7 @@ const Achievement = db.define('achievement', {
     primaryKey: true,
   },
   name: Sequelize.STRING,
-});
+}, { timesstamps: true });
 
 const joinFavoriteQuestion = db.define('join_favorite_question', {
   user_id: {
@@ -87,7 +89,7 @@ const joinFavoriteQuestion = db.define('join_favorite_question', {
     type: Sequelize.INTEGER,
     references: { model: Question, key: 'id' },
   },
-});
+}, { timesstamps: true });
 
 const joinAchievement = db.define('join_achievement', {
   user_id: {
@@ -98,25 +100,25 @@ const joinAchievement = db.define('join_achievement', {
     type: Sequelize.INTEGER,
     references: { model: Achievement, key: 'id' },
   },
-});
+}, { timesstamps: true });
 
-const testBoy = User.build({
-  username: 'maidenwench',
-  firstname: 'Robert',
-  lastname: 'Bartleby',
-  highscore: 22,
-  art_score: 5,
-  celebrities_score: 3,
-  animals_score: 0,
-  music_score: 66,
-  sports_score: 13,
-  books_score: 0,
-  myth_score: 17,
-  history_score: 85,
-  nature_score: 1,
-  politics_score: 13,
-});
-testBoy.save();
+// const testBoy = User.build({
+//   username: 'maidenwench',
+//   firstname: 'Robert',
+//   lastname: 'Bartleby',
+//   highscore: 22,
+//   art_score: 5,
+//   celebrities_score: 3,
+//   animals_score: 0,
+//   music_score: 66,
+//   sports_score: 13,
+//   books_score: 0,
+//   myth_score: 17,
+//   history_score: 85,
+//   nature_score: 1,
+//   politics_score: 13,
+// });
+// testBoy.save();
 
 db.sync()
   .then(() => {
