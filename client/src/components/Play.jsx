8a763237d0cbd464a22cdd049976fe4cbff695df
neuenhaus14 <neuenhaus14@gmail.com>
2023-11-05 17:30:44 -0600

    built Play skeleton, added event handling to select cats and difficulties

--- client/src/components/Play.jsx
+++ client/src/components/Play.jsx
@@ -1,5 +1,67 @@
-const Play = () => {
+import axios from 'axios';
+import React, { useState } from 'react';
 
+/**
+   * send get request to the API to load the selected choices (category and difficulty)
+   * choices will come from selection 
+   * selections will be handled by onChange on the SELECT input not option
+   * update state to whatever the value is
+*/ 
+const Play = () => {
+  const [category, setCategory] = useState('');
+  const [difficulty, setDifficulty] = useState('');
+
+  console.log(category, difficulty);
+
+  const handlePlayClick = () => { //get request
+    axios.get('/api/play', {
+      category: { category },
+      difficulty: { difficulty },
+    })
+      .then(() => {
+        //render them the page w component did mount
+        console.log('GET trivia questions successful');
+      })
+      .catch((err) => {
+        console.error('GET trivia questions NOT successful', err);
+      });
+  };
+
+  const onCategorySelection = (event) => {
+    setCategory(event.currentTarget.value);
+  };
+
+  const onDifficultySelection = (event) => {
+    setDifficulty(event.currentTarget.value);
+  };
+
+  // const componentDidMount = () => {
+  //   handlePlayClick();
+  // };  
+
+  return (
+    <div>
+      <h3>Choose your Category and Difficulty Level</h3>
+      <select name="Category" id="Cat" onChange={(event) => onCategorySelection(event)}>
+        <option>Animals</option>
+        <option>Art</option>
+        <option>Books</option>
+        <option>Celebrities</option>
+        <option>History</option>
+        <option>Music</option>
+        <option>Mythology</option>
+        <option>Nature</option>
+        <option>Politics</option>
+        <option>Sports</option>
+      </select>
+      <select name="Difficulty" id="Difficulty" onChange={(event) => onDifficultySelection(event)}>
+        <option>Easy</option>
+        <option>Medium</option>
+        <option>Hard</option>
+      </select>
+      <button type="button" onClick={() => handlePlayClick()}>Play!</button>
+    </div>
+  );
 };
 
 export default Play;
