59b175c49dd6211b45a9118886f0694b8892f6e4
rvierbuc <ranlikerats@gmail.com>
2023-11-06 12:07:45 -0600

    QuestionBuilder, added a post request that submits questions to database in server app. Altered format of questions in db/index schema and seedbuilder.

--- client/src/components/QuestionBuilder.jsx
+++ client/src/components/QuestionBuilder.jsx
@@ -1,5 +1,107 @@
-const QuestionBuilder = () => {
+import React, { useState } from 'react';
+import axios from 'axios';
 
+const QuestionBuilder = () => {
+  const [question, setQuestion] = useState('');
+  const [correct_answer, setCorrectAnswer] = useState('');
+  const [incorrect_answer_1, setincorrectAnswer1] = useState('');
+  const [incorrect_answer_2, setincorrectAnswer2] = useState('');
+  const [incorrect_answer_3, setincorrectAnswer3] = useState('');
+  const [question_set, setQuestionSet] = useState('');
+  
+  const clearForm = () => {
+    setQuestion('');
+    setCorrectAnswer('');
+    setincorrectAnswer1('');
+    setincorrectAnswer2('');
+    setincorrectAnswer3('');
+    setQuestionSet('');
+  };
+  
+  const submitValue = () => {
+    const postQuestion = (questionContents) => {
+      axios.post('/createQuestion', questionContents)
+        .then((response) => {
+          console.log('Question Posted From QB: ', response.data);
+          clearForm();
+        })
+        .catch((err) => {
+          console.error('error saving question: ', err);
+        });
+    };
+
+    const questionBuilderValues = {
+      question,
+      correct_answer,
+      incorrect_answer_1,
+      incorrect_answer_2,
+      incorrect_answer_3,
+      question_set,
+      user_id: 1,
+    };
+    postQuestion(questionBuilderValues);
+  };
+  
+  return (
+    <span>
+      <div>
+        Question: 
+        {' '}
+        <input 
+          type="text"
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+        />
+      </div>
+      <div>
+        Correct Answer: 
+        {' '}
+        <input 
+          type="text"
+          value={correct_answer}
+          onChange={(e) => setCorrectAnswer(e.target.value)}
+        />
+      </div>
+      <div>
+        First Incorrect Answer: 
+        {' '}
+        <input 
+          type="text"
+          value={incorrect_answer_1}
+          onChange={(e) => setincorrectAnswer1(e.target.value)}
+        />     
+      </div>
+      <div>
+        Second Incorrect Answer: 
+        {' '}
+        <input 
+          type="text"
+          value={incorrect_answer_2}
+          onChange={(e) => setincorrectAnswer2(e.target.value)}
+        />
+        <div>
+          Third Incorrect Answer: 
+          {' '}
+          <input 
+            type="text"
+            value={incorrect_answer_3}
+            onChange={(e) => setincorrectAnswer3(e.target.value)}
+          />
+        </div>
+        <div>
+          Question Set: 
+          {' '}
+          <input 
+            type="text"
+            value={question_set}
+            onChange={(e) => setQuestionSet(e.target.value)}
+          />
+        </div>
+        <button type="button" onClick={submitValue}>Submit</button>
+      </div>
+      
+    </span>
+  );
 };
 
 export default QuestionBuilder;
--- server/app.js
+++ server/app.js
@@ -14,4 +14,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(clientPath, 'index.html'));
 });
 
+app.post('/createQuestion', (req, res) => {
+  Question.create(req.body)
+    .then((newQuestion) => {
+      res.status(201).json(newQuestion);
+    })
+    .catch((err) => {
+      console.error('error submitting question: ', err);
+      res.sendStatus(500).json({ error: 'server side error saving question' });
+    });
+});
+
 module.exports = app;
