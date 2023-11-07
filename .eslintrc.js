67afc6a4fa76eef1f3bb700cc4988f9f3b76a7dc
rvierbuc <ranlikerats@gmail.com>
2023-11-07 13:37:17 -0600

    disabled proptypes in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -55,6 +55,7 @@ module.exports = {
     'react/function-component-definition': 0,
     'arrow-body-style': 0,
     'no-unused-vars': 0,
-    'no-plusplus': 0
+    'no-plusplus': 0,
+    "react/prop-types": 0,
   },
 };
