61e14572f8c2dbd9a70cd0438d8203c23aee45df
rvierbuc <ranlikerats@gmail.com>
2023-11-08 16:15:01 -0600

    Adds a client component that allows editing user created quizes and saves them to the db.

--- client/src/components/questionBuilderComponents/editExistingQuiz.jsx
+++ client/src/components/questionBuilderComponents/editExistingQuiz.jsx
@@ -1,20 +1,34 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import ContentEditable from 'react-contenteditable';
 
 const EditExistingQuiz = ({ quizNames, id }) => {
-  console.log('quiznames in edit existing quiz:', quizNames);
   const [selectedQuiz, setSelectedQuiz] = useState([]);
-  const [editedQuiz, setEditedQuiz] = useState([]);
   
+  const handleQuizEdit = (index, key, value) => {
+    const updatedQuiz = [...selectedQuiz];
+    updatedQuiz[index][key] = value;
+    setSelectedQuiz(updatedQuiz);
+  };
+
   const handleQuizSelect = (quizName) => {
     axios.post('/retrieveUserQuiz/1', { question_set: quizName })
       .then(({ data }) => {
-        console.log('data in get:', data);
+        console.log('data in retrive quiz:', data);
         setSelectedQuiz(data);
       })
       .catch((err) => console.error('error getting quiz: ', err));
   };
+  console.log('selectedQuiz on render: ', selectedQuiz);
 
+  const handleSubmit = () => {
+    axios
+      .patch(`/updateUserQuiz/${id}`, selectedQuiz)
+      .then((response) => {
+        console.log('questions updated as: ', selectedQuiz);
+      })
+      .catch((err) => console.error('Error on client side submitting updated questions: ', err));
+  };
   return (
     <div>
       <h1>Edit Your Quizzes</h1>
@@ -37,39 +51,52 @@ const EditExistingQuiz = ({ quizNames, id }) => {
           <h2>Quiz Name: {selectedQuiz[0].question_set}</h2>
           {selectedQuiz.map((quiz, index) => (
             <div key={quiz.id}>
-              <p><b>{index + 1}.</b>
-                <span 
-                  contentEditable
-                >{quiz.question}
+              <div>
+                <span><b>{index + 1}.</b>
+                  <ContentEditable
+                    html={quiz.question}
+                    onChange={(e) => handleQuizEdit(index, 'question', e.target.value)}
+                  />
                 </span>
-              </p>
-              <p><b>Correct Answer:</b>
-                <span 
-                  contentEditable
-                >{quiz.correct_answer}
+              </div>
+              <div>
+                <span><b>Correct Answer:</b>
+                  <ContentEditable
+                    html={quiz.correct_answer}
+                    onChange={(e) => handleQuizEdit(index, 'correct_answer', e.target.value)}
+                  />
                 </span>
-              </p>
-              <p><b>Incorrect Answer 1:</b> 
-                <span 
-                  contentEditable
-                >{quiz.incorrect_answer_1}
+              </div>
+              <div>
+                <span><b>Incorrect Answer 1:</b> 
+                  <ContentEditable
+                    html={quiz.incorrect_answer_1}
+                    onChange={(e) => handleQuizEdit(index, 'incorrect_answer_1', e.target.value)}
+                  />
+                
                 </span>
-              </p>
-              <p><b>Incorrect Answer 2:</b> 
-                <span 
-                  contentEditable
-                  onInput={(e) => setEditedQuestions({})}
-                >{quiz.incorrect_answer_2}
+              </div>
+              <div>
+                <span><b>Incorrect Answer 2:</b> 
+                  <ContentEditable
+                    html={quiz.incorrect_answer_2}
+                    onChange={(e) => handleQuizEdit(index, 'incorrect_answer_2', e.target.value)}
+                  />
                 </span>
-              </p>
-              <p><b>Incorrect Answer 3:</b> 
-                <span 
-                  contentEditable
-                >{quiz.incorrect_answer_3}
+              </div>
+              <div>
+                <span><b>Incorrect Answer 3:</b> 
+                  <ContentEditable
+                    html={quiz.incorrect_answer_3}
+                    onChange={(e) => handleQuizEdit(index, 'incorrect_answer_3', e.target.value)}
+                  />
                 </span>
-              </p>
+              </div>
             </div>
           )) }
+          <div>
+            <button type="button" onClick={handleSubmit}>Save</button>
+          </div>
         </div>
       ) : null}
     </div>
