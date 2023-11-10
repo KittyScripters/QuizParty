d090d5e2f35a19e0c5f864e9571839f1fdba12d2
SNelson723 <stephenn649@gmail.com>
2023-11-10 16:00:22 -0600

    Adding style to profile feature, still need to add a few more words to the page to make the user experience less confusing

--- client/src/components/profileTabs/StatsTab.jsx
+++ client/src/components/profileTabs/StatsTab.jsx
@@ -10,7 +10,7 @@ const StatsTab = ({ stats }) => {
     [stats.celebrities_score, 'Celebrities'],
     [stats.history_score, 'History'],
     [stats.music_score, 'Music'],
-    [stats.myth_score, 'Mythology'],
+    [stats.mythology_score, 'Mythology'],
     [stats.nature_score, 'Nature'],
     [stats.politics_score, 'Politics'],
     [stats.sports_score, 'Sports'],
@@ -20,34 +20,39 @@ const StatsTab = ({ stats }) => {
     return `${scores[0][1]}: ${scores[0][0]}`;
   };
   return (
-    <div id="profile-scores">
-      <div id="score-header"><h3>Best Category: {checkHighestCatScore(catScores)}</h3></div>
-      <table className="stats">
+    <div
+      id="profile-scores"
+      className="
+        text-center
+      "
+    >
+      <div className="start-50" id="score-header"><h3>Best Category: {checkHighestCatScore(catScores)}</h3></div>
+      <table className="start-50">
         <tr className="category">
-          <th className="stats-headers">Total</th>
-          <th className="stats-headers">Animals</th>
-          <th className="stats-headers">Art</th>
-          <th className="stats-headers">Books</th>
-          <th className="stats-headers">Celebrities</th>
-          <th className="stats-headers">History</th>
-          <th className="stats-headers">Music</th>
-          <th className="stats-headers">Mythology</th>
-          <th className="stats-headers">Nature</th>
-          <th className="stats-headers">Politics</th>
-          <th className="stats-headers">Sports</th>
+          <th id="stats-total" className="">Total</th>
+          <th className="">Animals</th>
+          <th className="">Art</th>
+          <th className="">Books</th>
+          <th className="">Celebrities</th>
+          <th className="">History</th>
+          <th className="">Music</th>
+          <th className="">Mythology</th>
+          <th className="">Nature</th>
+          <th className="">Politics</th>
+          <th className="">Sports</th>
         </tr>
-        <tr className="user-scores">
-          <td className="stats-score">{stats.highscore}</td>
-          <td className="stats-score">{stats.animals_score}</td>
-          <td className="stats-score">{stats.art_score}</td>
-          <td className="stats-score">{stats.books_score}</td>
-          <td className="stats-score">{stats.celebrities_score}</td>
-          <td className="stats-score">{stats.history_score}</td>
-          <td className="stats-score">{stats.music_score}</td>
-          <td className="stats-score">{stats.myth_score}</td>
-          <td className="stats-score">{stats.nature_score}</td>
-          <td className="stats-score">{stats.politics_score}</td>
-          <td className="stats-score">{stats.sports_score}</td>
+        <tr id="stats-total" className="user-scores">
+          <td className="">{stats.highscore}</td>
+          <td className="">{stats.animals_score}</td>
+          <td className="">{stats.art_score}</td>
+          <td className="">{stats.books_score}</td>
+          <td className="">{stats.celebrities_score}</td>
+          <td className="">{stats.history_score}</td>
+          <td className="">{stats.music_score}</td>
+          <td className="">{stats.mythology_score}</td>
+          <td className="">{stats.nature_score}</td>
+          <td className="">{stats.politics_score}</td>
+          <td className="">{stats.sports_score}</td>
         </tr>
       </table>
     </div>
